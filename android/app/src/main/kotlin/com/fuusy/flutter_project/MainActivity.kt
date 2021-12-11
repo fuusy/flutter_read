ca1d04dda3e2ab6a98ab374a08c810d1fa8c0244
fuusy <643432857@qq.com>
2021-12-11 21:44:58 +0800

    fix：启动页状态栏显示灰色闪动

--- android/app/src/main/kotlin/com/fuusy/flutter_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/fuusy/flutter_project/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.fuusy.flutter_project
 
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-
-
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = activity.window
+            //设置状态栏为透明色，
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
 }
